c0eba5cf6e3ee26f5bf90b7f786bcc1a521d35a2
dr1vesafe <snddadaya@gmail.com>
2025-05-29 17:39:13 +0400

    Созданы модели Genre, Author, Book.

---
+++ task_1/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from database import Base
+
+
+class Genre(Base):
+    __tablename__ = "genre"
+
+    genre_id = Column(Integer, primary_key=True)
+    name_genre = Column(String)
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    author_id = Column(Integer, primary_key=True)
+    name_author = Column(String)
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey("author.author_id"))
+    genre_id = Column(Integer, ForeignKey("genre.genre_id"))
+    price = Column(Float)
+    amount = Column(Integer)
